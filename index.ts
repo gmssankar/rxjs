4f93e2acb1a3511075f0a3413f8534533a28a47a
gmssankar <gmssankar@gmail.com>
2019-10-19 12:51:56 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,23 @@
-import { fromEvent } from 'rxjs';
-import { scan } from 'rxjs/operators';
+import { fromEvent,interval} from 'rxjs';
+import { scan,pluck,groupBy } from 'rxjs/operators';
 
-fromEvent(document, 'click')
-  .pipe(scan(count => count + 1, 0))
-  .subscribe(count => console.log(`Clicked ${count} times`));
+// fromEvent(document, 'click')
+//   .pipe(scan(count => count + 1, 0))
+//   .subscribe(count => console.log(`Clicked ${count} times`));
+
+
+
+//const { interval } = Rx;
+//const { scan, pluck, groupBy } = RxOperators;
+
+interval(1000).pipe(
+  scan(
+    ({ secondLast, last }) => ({
+      secondLast: last,
+      last: last + secondLast
+    }),
+    { secondLast: 0, last: 1 }
+  ),
+  pluck("secondLast"),
+  groupBy(n => Math.floor(Math.log10(n)))
+)
